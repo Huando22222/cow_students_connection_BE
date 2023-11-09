0ef8ffe350a40af3b609d8fa446036d361721bb3
HuanDo22222 <huando22222@gmail.com>
2023-11-09 12:52:50 +0700

    ad

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -11,22 +11,14 @@ module.exports = {
 			// const user = await Users.find().sort({ createdAt: -1 });    //send array object
 			// const userId = "65475c7b622dee003c7d60f0";         
 			// const user = await Users.findOne({ _id: userId });          //only 1 user
-			const user = await Account.findOne({
-				   phone: phone ,  password: password ,
-			  });
 			console.log(
-				user+
 				 "----------------test--------------------------"
 			);
 			// const data = { user: user, token: "token1234567890abc" };
 
 			// res.status(200).json({ message: "OK", data: user });
-			
-			if (user !== null) {
-				res.status(200).json({ message: "OK", data: user });
-			  } else {
-				res.status(500).json("Loi Dang Nhap");
-			  }
+			res.status(200).json({ message: "OK"});
+
 			// const { username, password } = req.body;
 			// console.log("username:  "+username);
 			// if (username === "123" && password === "123") {
@@ -36,7 +28,7 @@ module.exports = {
 			//     res.status(500).json("incorre"${AppConfig.baseUrl}""${AppConfig.baseUrl}"ct username or password");
 			// }
 		} catch (error) {
-            res.status(500).json("false load user");
+            // res.status(500).json("false load user");
         }
     },
 
@@ -62,12 +54,10 @@ module.exports = {
 			);
 			const user = await Account.findOne({
 				$or: [
-				  { $and: [{ phone: phone }, { id: "" }] },
-				  { $and: [{ id: id },{ id: { $ne: "" } }]},
-				]
-			  });
-			  
-			  
+					{ $and: [{ phone: phone }, { id: { $exists: false } }] },
+					{ id: { $exists: true } },
+				],
+			});
 			console.log(user);
 			if (user !== null) {
 				//return acc already exists
@@ -97,6 +87,63 @@ module.exports = {
 					})
 					.catch((err) => console.log(err));
 			}
+			// if (phone !== null && password !== null) {
+			// 	const user = await Account.findOne({ phone: phone });
+			// 	if (user !== null) {
+			// 		//return acc already exists
+			// 		console.log("acc already exists!" );
+			// 		res.status(500).json({
+			// 			message: "acc already exists",
+			// 		});
+			// 	} else {
+			// 		const account = new Account({
+			// 			phone,
+			// 			password,
+			// 			displayName: "",
+			// 			email: "",
+			// 			id: "",
+			// 			photoUrl: "",
+			// 			// serverAuthCode: "",
+			// 		});
+
+			// 		// Lưu tài khoản vào cơ sở dữ liệu
+			// 		await account
+			// 			.save()
+			// 			.then(() =>
+			// 				console.log("Đã đăng ký tài khoản thành công")
+			// 			)
+			// 			.catch((err) => console.log(err));
+			// 	}
+			// } else {
+			// 	const user = await Account.findOne({ id: id });
+			// 	if (user !== null) {
+			// 		//return acc already exists
+			// 		console.log("acc already exists!");
+			// 		res.status(500).json({
+			// 			message: "acc already exists",
+			// 		});
+			// 	} else {
+			// 		const account = new Account({
+			// 			phone: "",
+			// 			password: "",
+			// 			displayName,
+			// 			email,
+			// 			id,
+			// 			photoUrl,
+			// 			// serverAuthCode,
+			// 		});
+
+			// 		// Lưu tài khoản vào cơ sở dữ liệu
+			// 		await account
+			// 			.save()
+			// 			.then(() =>
+			// 				console.log("Đã đăng ký tài khoản thành công")
+			// 			)
+			// 			.catch((err) => console.log(err));
+			// 	}
+			// }
+	  
+		  
 		} catch (error) {
 		  res.status(500).json({ message: "Lỗi trong quá trình đăng ký tài khoản" });
 		}
