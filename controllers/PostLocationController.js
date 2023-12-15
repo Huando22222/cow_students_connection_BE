7df97fe7e5d5c874ba02fcd1572c588a9835c696
ATuns <vip37atuan@gmail.com>
2023-12-15 17:58:16 +0700

    postLocationstLocationF

--- controllers/PostLocationController.js
+++ controllers/PostLocationController.js
@@ -2,18 +2,40 @@ const PostLocation = require('../models/PostLocations');
 const User = require('../models/Users');
 
 module.exports = {
+    DeletePostLocation: async (req, res) => {
+        try {
+          const { postId } = req.body;
+          const updatedPostLocation = await PostLocation.findByIdAndUpdate(
+            postId,
+            { isDeleted: true }, // Update isDeleted directly to true
+            { new: true }
+          ).populate("owner");
+    
+          if (!updatedPostLocation) {
+            return res.status(404).json({ message: "Post not found" });
+          }
+    
+          res.status(200).json({
+            message: "Post location marked as deleted successfully",
+            postLocation: updatedPostLocation,
+          });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: "Server Error" });
+        }
+      },
   NewPostLocation: async (req, res) => {
     try {
 
       const { owner, message, latitude, longitude } = req.body;
-      console.log(owner+ "- "+ message+ "- "+ latitude+"- "+ longitude);
+      console.log(owner+ "- "+ message+ "- "+ latitude+"- "+ longitude+"- ");
       const postLocation = new PostLocation({
         owner: owner,
         message: message,
         location: {
             latitude:latitude,
             longitude:longitude,
-        }
+        },
       });
 
       await postLocation.save();
@@ -31,11 +53,14 @@ module.exports = {
 
   GetPostLocations: async (req, res) => {
     try {
-      const postLocations = await PostLocation.find().populate("owner").sort({ createdAt: -1 });
-      res.status(200).json({  data: postLocations });
+      const postLocations = await PostLocation.find({ isDeleted: false })
+        .populate("owner")
+        .sort({ createdAt: -1 });
+      res.status(200).json({ data: postLocations });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Server Error" });
     }
   },
+  
 };
